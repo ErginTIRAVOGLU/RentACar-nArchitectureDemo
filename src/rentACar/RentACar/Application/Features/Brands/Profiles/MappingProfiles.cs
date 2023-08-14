32cbcb74c3aedfa744b7d51d18b3c12710f4bb14
Ergin TIRAVOGLU <ergintiravoglu@msn.com>
2023-08-15 00:42:01 +0300

    Update Command CQRS Implementasyonu Eklendi

--- src/rentACar/RentACar/Application/Features/Brands/Profiles/MappingProfiles.cs
+++ src/rentACar/RentACar/Application/Features/Brands/Profiles/MappingProfiles.cs
@@ -1,4 +1,5 @@
 ﻿using Application.Features.Brands.Commands.Create;
+using Application.Features.Brands.Commands.Update;
 using Application.Features.Brands.Queries.GetById;
 using Application.Features.Brands.Queries.GetList;
 using AutoMapper;
@@ -19,8 +20,14 @@ public class MappingProfiles:Profile
     {
         CreateMap<Brand, CreateBrandCommand>().ReverseMap();
         CreateMap<Brand, CreatedBrandResponse>().ReverseMap();
-        CreateMap<Brand, GetListBrandListItemDto>().ReverseMap();
+        
+        CreateMap<Brand, GetListBrandListItemDto>().ReverseMap();        
+        
         CreateMap<Brand, GetByIdBrandResponse>().ReverseMap();
+        
+        CreateMap<Brand, UpdateBrandCommand>().ReverseMap();        
+        CreateMap<Brand, UpdatedBrandResponse>().ReverseMap();
+
         CreateMap<Paginate<Brand>, GetListResponse<GetListBrandListItemDto>>().ReverseMap(); 
     }
 }
