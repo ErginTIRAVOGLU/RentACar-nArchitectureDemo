d09a25a9f0bdac3b201645191d6c21bec3f2d371
Ergin TIRAVOGLU <ergintiravoglu@msn.com>
2023-08-17 02:57:46 +0300

    MediatR Pipeline ile Transaction Implementasyonu tamamlandı

--- src/rentACar/RentACar/Application/ApplicationServiceRegistration.cs
+++ src/rentACar/RentACar/Application/ApplicationServiceRegistration.cs
@@ -1,4 +1,5 @@
-﻿using Core.Application.Pipelines.Validation;
+﻿using Core.Application.Pipelines.Transaction;
+using Core.Application.Pipelines.Validation;
 using Core.Application.Rules;
 using FluentValidation;
 using Microsoft.Extensions.DependencyInjection;
@@ -24,7 +25,10 @@ public static class ApplicationServiceRegistration
         {
             configuration.RegisterServicesFromAssembly(Assembly.GetExecutingAssembly());
             configuration.AddOpenBehavior(typeof(RequestValidationBehavior<,>));
+            configuration.AddOpenBehavior(typeof(TransactionScopeBehavior<,>));
+
         });
+
 
         return services;
     }
--- src/rentACar/RentACar/Application/Features/Brands/Commands/Create/CreateBrandCommand.cs
+++ src/rentACar/RentACar/Application/Features/Brands/Commands/Create/CreateBrandCommand.cs
@@ -1,6 +1,7 @@
 ﻿using Application.Features.Brands.Rules;
 using Application.Services.Repositories;
 using AutoMapper;
+using Core.Application.Pipelines.Transaction;
 using Domain.Entities;
 using MediatR;
 using System;
@@ -11,7 +12,7 @@ using System.Threading.Tasks;
 
 namespace Application.Features.Brands.Commands.Create;
 
-public class CreateBrandCommand : IRequest<CreatedBrandResponse>
+public class CreateBrandCommand : IRequest<CreatedBrandResponse>,ITransactionalRequest
 {
     public string Name { get; set; }
 
@@ -36,8 +37,8 @@ public class CreateBrandCommand : IRequest<CreatedBrandResponse>
 
             Brand brand = _mapper.Map<Brand>(request);
             brand.Id = Guid.NewGuid();
-
             var result = await _brandRepository.AddAsync(brand);
+
 
             CreatedBrandResponse createdBrandResponse = _mapper.Map<CreatedBrandResponse>(result);            
             return createdBrandResponse;
