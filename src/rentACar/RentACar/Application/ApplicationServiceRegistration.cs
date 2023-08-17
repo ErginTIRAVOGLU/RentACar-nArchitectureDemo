2d080243980cf9291f257bb4d8dd9ce5c067d8d1
Ergin TIRAVOGLU <ergintiravoglu@msn.com>
2023-08-17 23:57:36 +0300

    Loglama Altyapısının Implementasyonu tamamlandı

--- src/rentACar/RentACar/Application/ApplicationServiceRegistration.cs
+++ src/rentACar/RentACar/Application/ApplicationServiceRegistration.cs
@@ -1,7 +1,10 @@
 ﻿using Core.Application.Pipelines.Caching;
+using Core.Application.Pipelines.Logging;
 using Core.Application.Pipelines.Transaction;
 using Core.Application.Pipelines.Validation;
 using Core.Application.Rules;
+using Core.CrossCuttingConserns.Serilog;
+using Core.CrossCuttingConserns.Serilog.Loggers;
 using FluentValidation;
 using Microsoft.Extensions.DependencyInjection;
 using System;
@@ -29,9 +32,11 @@ public static class ApplicationServiceRegistration
             configuration.AddOpenBehavior(typeof(TransactionScopeBehavior<,>));
             configuration.AddOpenBehavior(typeof(CachingBehavior<,>));
             configuration.AddOpenBehavior(typeof(CacheRemovingBehavior<,>));
+            configuration.AddOpenBehavior(typeof(LoggingBehavior<,>));
 
         });
 
+        services.AddSingleton<LoggerServiceBase, FileLogger>();
 
         return services;
     }
